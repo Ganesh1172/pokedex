24c7674db7a1954f74f7de642206d7b23744d57c
ganesh1172 <ganesh152595@gmail.com>
2020-04-18 20:05:07 +0530

    Updates

---
+++ precache-manifest.86627995108d690a791abd9ca7ea74bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68a61ad679e5292ba865f2b7771309d2",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "3fa3929d73d6f5039fe4",
+    "url": "/pokedex/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ef013c6b52e8e20bd309",
+    "url": "/pokedex/static/js/2.4992347f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.4992347f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fa3929d73d6f5039fe4",
+    "url": "/pokedex/static/js/main.6f952237.chunk.js"
+  },
+  {
+    "revision": "601f8d113175821311bd",
+    "url": "/pokedex/static/js/runtime-main.0ec6b405.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pokedex/static/media/logo.5d5d9eef.svg"
+  }
+]);
